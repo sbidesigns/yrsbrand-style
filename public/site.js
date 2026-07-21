41f03e74d297061e6ea694c30517a4391a1157cc
Z User <z@container>
2026-07-21 17:02:54 +0000

    77e1646a-34ef-4217-a741-0562889a7ea5

---
+++ public/site.js
@@ -0,0 +1,141 @@
+/**
+ * site.js — YRS Brand content & data
+ * All post content, brand info, navigation links live here.
+ * Edit this file to update what the site shows.
+ */
+
+var SITE_DATA = {
+  "brand": "YRS",
+  "description": "YRS\u00AE Brand. <br><br>We create apparel that motivates and inspires the individual to be great &amp; Enjoy life while you listen to your favorite music, skateboarding down the street, or in flight to your next destination, &amp; inspire people along the way. There's too much to be thankful for \u2014 If you're high, never come down &amp; Stay Fresh!<br><br>Shop The Official YRS\u00AE Brand Online Store for a wide range of exclusive apparel and custom footwear.  -everythingisYRS'",
+  "links": [
+    {
+      "label": "lifestyle",
+      "href": "/",
+      "title": "lifestyle"
+    },
+    {
+      "label": "music & videos",
+      "href": "/",
+      "title": "music & videos"
+    },
+    {
+      "label": "[ shop ]",
+      "href": "http://www.shopyrs.com",
+      "title": "[ shop ]",
+      "external": true
+    },
+    {
+      "label": "connect",
+      "href": "#connect",
+      "title": "connect",
+      "panel": "connect"
+    }
+  ],
+  "social": [
+    {
+      "label": "Facebook",
+      "href": "https://www.facebook.com/yrsbrand",
+      "svg": '<svg width="16" height="16" viewBox="0 0 24 24" fill="#000"><path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/></svg>'
+    },
+    {
+      "label": "Twitter",
+      "href": "https://www.twitter.com/yrsbrand",
+      "svg": '<svg width="16" height="16" viewBox="0 0 24 24" fill="#000"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg>'
+    },
+    {
+      "label": "Instagram",
+      "href": "https://www.instagram.com/yrsbrand",
+      "svg": '<svg width="16" height="16" viewBox="0 0 24 24" fill="#000"><path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98C8.333 23.986 8.741 24 12 24c3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 100 12.324 6.162 6.162 0 000-12.324zM12 16a4 4 0 110-8 4 4 0 010 8zm6.406-11.845a1.44 1.44 0 100 2.881 1.44 1.44 0 000-2.881z"/></svg>'
+    }
+  ],
+  "connectInfo": "Follow YRS\u00AE Brand across all platforms for updates, drops, and behind-the-scenes content.",
+  "posts": [
+    {
+      "type": "reblog",
+      "html": "<p><a href=\"https://supportthehomegrown.tumblr.com/post/643966276151246848\" class=\"tumblr_blog\">supportthehomegrown</a>:</p><blockquote><p></p><figure class=\"tmblr-full\" data-orig-height=\"1000\" data-orig-width=\"1000\"><a href=\"https://supportthehomegrown.bigcartel.com/product/support-dark-green-white\"><img src=\"https://64.media.tumblr.com/6cb29173bbd144ebe18af62b31fa303c/ce558bb518e63301-aa/s500x750/a820392b788a9e24199ded58f34ccfac0c0784f8.png\" data-orig-height=\"1000\" data-orig-width=\"1000\" width=\"500\" height=\"500\" alt=\"image\"></a></figure><a href=\"https://supportthehomegrown.bigcartel.com/product/support-light-green-black\"><figure class=\"tmblr-full\" data-orig-height=\"1000\" data-orig-width=\"1000\"><img src=\"https://64.media.tumblr.com/8470101cd471b2f437110c47887482a4/ce558bb518e63301-b0/s500x750/abbe186977612ec72ff52a3b01224381e8601932.png\" data-orig-height=\"1000\" data-orig-width=\"1000\" width=\"500\" height=\"500\" alt=\"image\"></figure></a><a href=\"https://supportthehomegrown.bigcartel.com/product/support-light-purp-black\"><figure class=\"tmblr-full\" data-orig-height=\"1000\" data-orig-width=\"1000\"><img src=\"https://64.media.tumblr.com/49cf7a733d85512af4357e6657064ced/ce558bb518e63301-8e/s500x750/8c2818901643c3d86e2315b345cdc162acd87c55.png\" data-orig-height=\"1000\" data-orig-width=\"1000\" width=\"500\" height=\"500\" alt=\"image\"></figure></a><a href=\"https://supportthehomegrown.bigcartel.com/product/support-dark-purp-white\"><figure class=\"tmblr-full\" data-orig-height=\"1000\" data-orig-width=\"1000\"><img src=\"https://64.media.tumblr.com/2dfd802f38db0df54a22c7465ad72ce4/ce558bb518e63301-50/s500x750/a8c9f26f15d6f068520b02116ceaf2719c505b6d.png\" data-orig-height=\"1000\" data-orig-width=\"1000\" width=\"500\" height=\"500\" alt=\"image\"></figure></a><p></p></blockquote>",
+      "date": "25.Feb.21",
+      "timeAgo": "5 years ago",
+      "url": "https://yrsbrand.tumblr.com/post/644132651600609280/supportthehomegrown"
+    },
+    {
+      "type": "photo",
+      "timeAgo": "5 years ago",
+      "img": "https://64.media.tumblr.com/2e5059bcd013b03eca865b26c05bc4e1/6496aa7bc41b1825-46/s500x750/e4a8f7dd42a5d6e02ce57cbe2bcf6c605126f268.jpg",
+      "notes": 12,
+      "date": "25.Feb.21",
+      "url": "https://yrsbrand.tumblr.com/post/644132596883898368/green-and-gold-hippie-tee-greengoldbrand",
+      "reblogUrl": "https://www.tumblr.com/reblog/yrsbrand/644132596883898368/a0yOK1YU"
+    },
+    {
+      "type": "photo",
+      "img": "https://64.media.tumblr.com/8b3e84d66bf86a98239fdb0dfe471e49/tumblr_n3x5jdZ0Xt1ttyzz9o1_500.jpg",
+      "notes": 5,
+      "date": "29.May.14",
+      "timeAgo": "12 years ago",
+      "url": "https://yrsbrand.tumblr.com/post/87204718074/yslpro-restocked-support-the-growth-check",
+      "reblogUrl": "https://www.tumblr.com/reblog/yrsbrand/87204718074/Cl3b407R"
+    },
+    {
+      "type": "photo",
+      "img": "https://64.media.tumblr.com/1957f9a3d9c890c951462481c08fc989/tumblr_n1u65utCFK1ttbukao1_500.png",
+      "notes": 2,
+      "date": "02.Mar.14",
+      "timeAgo": "12 years ago",
+      "url": "https://yrsbrand.tumblr.com/post/78383765051/paradisimo",
+      "reblogUrl": "https://www.tumblr.com/reblog/yrsbrand/78383765051/y8x4woKD"
+    },
+    {
+      "type": "photo",
+      "img": "https://64.media.tumblr.com/8c952fe8c2c6acbe545d923b585b02f3/tumblr_n1u5z7uJVE1ttbukao1_500.jpg",
+      "notes": 2,
+      "date": "02.Mar.14",
+      "timeAgo": "12 years ago",
+      "url": "https://yrsbrand.tumblr.com/post/78383365965/100-authentic-limited-edition-custom-jordans-now",
+      "reblogUrl": "https://www.tumblr.com/reblog/yrsbrand/78383365965/5ZJNSykh"
+    },
+    {
+      "type": "photo",
+      "img": "https://64.media.tumblr.com/12446b26fe34ec1d04998f295041e459/tumblr_n1m5b5XYQE1ttbukao1_500.jpg",
+      "notes": 1,
+      "date": "26.Feb.14",
+      "timeAgo": "12 years ago",
+      "url": "https://yrsbrand.tumblr.com/post/77917837235/yrsbrand-bimmer",
+      "reblogUrl": "https://www.tumblr.com/reblog/yrsbrand/77917837235/3uEz5Obv"
+    },
+    {
+      "type": "photo",
+      "img": "https://64.media.tumblr.com/249d6d5f8b65a14f193d3c852875050f/tumblr_n1gwvoOMeT1ttbukao1_500.png",
+      "notes": 5,
+      "date": "23.Feb.14",
+      "timeAgo": "12 years ago",
+      "url": "https://yrsbrand.tumblr.com/post/77628704000",
+      "reblogUrl": "https://www.tumblr.com/reblog/yrsbrand/77628704000/ZdaVOoyy"
+    },
+    {
+      "type": "photo",
+      "img": "https://64.media.tumblr.com/b9aa0a309ce658604000954498dd1ef5/tumblr_n1f5ebYxTa1ttbukao1_500.jpg",
+      "notes": 7,
+      "date": "22.Feb.14",
+      "timeAgo": "12 years ago",
+      "url": "https://yrsbrand.tumblr.com/post/77520134567",
+      "reblogUrl": "https://www.tumblr.com/reblog/yrsbrand/77520134567/sdn6LpOJ"
+    },
+    {
+      "type": "link",
+      "title": "Quote Of The Day - GOOD VIBES",
+      "href": "https://www.goodvibes.ga/quote-of-the-day/",
+      "date": "21.Jan.21",
+      "timeAgo": "5 years ago",
+      "url": "https://yrsbrand.tumblr.com/post/640897384951496704/quote-of-the-day-good-vibes"
+    },
+    {
+      "type": "video",
+      "src": "https://www.youtube.com/embed/5L4LvlJZhBs?feature=oembed&enablejsapi=1&wmode=opaque",
+      "title": "Starr L.PRO - YRS Brand (Fly Shit Only) (PPV) #LGNDARY",
+      "date": "23.Feb.14",
+      "timeAgo": "12 years ago",
+      "url": "https://yrsbrand.tumblr.com/post/77574370340/young-starr-lpro-youngstarrtm-yrs-brand"
+    }
+  ],
+  "footer": "<font style=\"font-family:Serif; font-size: 50px;letter-spacing:-1px;font-weight:600\">YRS\u00AE</font><br>\u00A9 2010-2026 YRS Brand"
+};
